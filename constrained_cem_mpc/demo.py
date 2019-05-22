c13d32c1e99091737a926b7e20b9797cf1e556e1
Oscar Key <oscar.t.key@gmail.com>
2019-05-22 17:52:59 +0100

    Add new demo objective.

--- constrained_cem_mpc/demo.py
+++ constrained_cem_mpc/demo.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import torch
+import torch.nn.functional as F
 
 from constrained_cem_mpc import ConstrainedCemMpc, TerminalConstraint, StateConstraint, ActionConstraint, box2torchpoly, \
     Rollout
@@ -10,7 +11,7 @@ action_dimen = 2
 x_max = 10
 y_max = 10
 
-objective_poly = box2torchpoly([[5, 6], [7, 8]])
+objective_poly = box2torchpoly([[1, 2], [8, 9]])
 safe_area = box2torchpoly([[0, 10], [0, 10]])
 terminal_constraint = box2torchpoly([[8, 9], [8, 9]])
 
@@ -23,10 +24,7 @@ def dynamics(s, a):
 
 
 def objective_cost(trajectory, _):
-    if objective_poly.contains_points(trajectory) > 0:
-        return 0
-    else:
-        return 50
+    return F.pairwise_distance(objective_poly.chebXc.unsqueeze(0), trajectory[10].unsqueeze(0))
 
 
 def check_intersect(t, c):
@@ -46,9 +44,9 @@ def plot_trajectories(ts, axes=None):
     axes.set_xticks([0, x_max])
     axes.set_yticks([0, y_max])
 
-    safe_area.plot(ax=axes)
-    objective_poly.plot(ax=axes)
-    terminal_constraint.plot(ax=axes)
+    safe_area.plot(ax=axes, color='lightgrey')
+    objective_poly.plot(ax=axes, color='palegreen')
+    terminal_constraint.plot(ax=axes, color='deepskyblue')
 
     for t in ts:
         xs = [s[0].item() for s in t]
@@ -73,6 +71,7 @@ def plot_costs(rollouts_by_time: [[Rollout]]):
     xs = range(1, len(rollouts_by_time) + 1)
     plt.plot(xs, avg_objective_costs, label='objective cost')
     plt.plot(xs, avg_constraint_costs, label='constraint cost')
+    plt.plot([0, 50], [0, 0], color='black')
     plt.legend()
     plt.show()
 
@@ -84,7 +83,7 @@ def main():
                    StateConstraint(safe_area),  #
                    ActionConstraint(box2torchpoly([[-1, 1], [-1, 1]]))]
     mpc = ConstrainedCemMpc(dynamics, objective_cost, constraints, state_dimen, action_dimen, time_horizon=20,
-                            num_rollouts=400, num_elites=20, num_iterations=30, num_workers=2)
+                            num_rollouts=400, num_elites=30, num_iterations=50, num_workers=2)
     rollouts_by_time = mpc.optimize_trajectories(torch.tensor([0.5, 0.5]))
 
     plot_trajectories([rollout.trajectory for rollout in rollouts_by_time[-1][0:10]])
